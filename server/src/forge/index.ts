071e0a2e6f7a9d7c036a1042726e7542ffaac4d1
DangJingtao <dangjingtao@163.com>
2026-09-05 23:06:57 +0800

    fix(forge): avoid duplicate barrel exports

--- server/src/forge/index.ts
+++ server/src/forge/index.ts
@@ -1,4 +1,23 @@
-export * from "./types.js";
+export type {
+  ForgeAdapter,
+  ForgeAdapterKind,
+  ForgeAdapterStatus,
+  ForgeBatch,
+  ForgeBatchStatus,
+  ForgeCoreState,
+  ForgeDispatch,
+  ForgeDispatchStatus,
+  ForgeProject,
+  ForgeReview,
+  ForgeReviewStatus,
+  ForgeRuntimeEvent,
+  ForgeSession,
+  ForgeSessionRole,
+  ForgeSessionStatus,
+  ForgeTask,
+  ForgeTaskStatus,
+} from "./types.js";
+export { createForgeCoreState } from "./types.js";
 export * from "./domain.js";
 export * from "./dispatch-domain.js";
 export * from "./readiness.js";
